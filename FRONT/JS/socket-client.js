0959c12dc2f93e8cf9c20a7e27aba14b4f6089d2
SamuelZ7021 <samuelandrey1207@gmail.com>
2025-08-20 12:20:01 -0500

    Conexion de Socket-Cleinte, agregacion Socket en el server.js

--- FRONT/JS/socket-client.js
+++ FRONT/JS/socket-client.js
@@ -1,1 +1,26 @@
 // socket-client.js - Archivo inicial
+
+// cliente-servidor
+import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
+
+const server_URL = import.meta.env.VITE_SOCKET_URL
+
+
+// Crear conexion
+export const socket = io(server_URL, {
+    transports: ["websocket"],
+    autoConnect: true
+});
+
+// Detectar conexion
+socket.on("connect", () => {
+    console.log("Conectado al servidor con ID:", socket.id);
+    
+    // Enviar evento ded prueba
+    socket.emit("testEvent", {msg: "Hola servidor, soy el cliente"});
+});
+
+// Rrcibir respuesta del servidor
+socket.on("testEventResponse", (data) => {
+    console.log("Respuesta del servidor:", data);
+});
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import http from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 import serviceRoutes from "./BACK/routes/serviceRoutes.js";
 import customerRoutes from "./BACK/routes/customerRoutes.js";
@@ -37,4 +39,40 @@ app.get("/health", (req, res) => {
     uptime: process.uptime(),
     dbState: mongoose.connection.readyState, // 1 = conectado
   });
+});
+
+
+
+// Socket.IO
+const server = http.createServer(app);
+
+// Usamos variables de entorno
+const Port = process.env.PORT || 3000;
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+
+const io = new Server(server, {
+  cors: {
+    origin: CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+// Evento de conexión
+io.on("connection", (socket) => {
+  console.log("Cliente conectado:", socket.id);
+
+  // Evento testEvent
+  socket.on("testEvent", (data) => {
+    console.log("testEvent recibido:", data);
+    socket.emit("testEventResponse", { msg: "Hola cliente, recibí tu evento" });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado:", socket.id);
+  });
+});
+
+// Iniciar servidor
+server.listen(Port, () => {
+  console.log(`Servidor corriendo en http://localhost:${Port}`);
 });
